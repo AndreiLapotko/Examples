505d2e75c27fbc17aa2714de3d86abc38ec2f8cf
AndreiLapotko <shudyx@gmail.com>
2023-09-17 09:11:55 +1000

    added Example 004 Div project

---
+++ Example_004_Div/Program.cs
@@ -0,0 +1,8 @@
+﻿//программа для деления двух чисел, получаемых от пользователя
+Console.WriteLine("Программа для деления двух чисел, получаемых от пользователя!");
+Console.Write("Введите первое число: ");
+double numberA = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double numberB = double.Parse(Console.ReadLine());
+double result = numberA / numberB;
+Console.WriteLine("Результат деления Ваших чисел равен: " + result);
