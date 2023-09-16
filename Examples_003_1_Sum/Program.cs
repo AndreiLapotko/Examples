12c0689d9c70c5fe813cc773e623b45eddf34550
AndreiLapotko <shudyx@gmail.com>
2023-09-17 09:31:29 +1000

    added Examples 003_1 Sum project

---
+++ Examples_003_1_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+//программа для суммирования двух случайно генерируемых чисел
+int numberA = new Random().Next(1, 10); //генерируется случайное целое число в диапазоне от 1 до 9 включительно
+int numberB = new Random().Next(1, 10);
+Console.WriteLine("Первое число: " + numberA);
+Console.WriteLine("Второе число: " + numberB);
+int result = numberA + numberB;
+Console.WriteLine("Сумма двух случайно генерируемых чисел равна: " + result);
