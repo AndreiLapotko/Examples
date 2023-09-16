62edf249cdad97dea0366fa8645d29d2b87dc2be
AndreiLapotko <shudyx@gmail.com>
2023-09-17 08:53:09 +1000

    added Example 003 Sum project

---
+++ Example_003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿//программа для суммирования двух чисел, получаемых от пользователя
+Console.WriteLine("Программа для суммирования двух чисел, получаемых от пользователя!");
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberA + numberB;
+Console.WriteLine("Сумма Ваших чисел равна: " + result);
